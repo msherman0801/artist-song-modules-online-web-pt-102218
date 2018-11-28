c4f9ad5af2c7e3015d9a31dc48990dc1e6aefa29
Matthew Sherman <msherman0801@gmail.com>
2018-11-28 22:34:30 +0000

    Automatically backed up by Learn

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,15 @@
+module Memorable
+
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|a| a.name == name}
+  end
+
+end
